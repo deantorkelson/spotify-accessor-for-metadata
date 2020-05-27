14cc08371c50ad2312d5b8b124e88ca5e4330329
deantorkelson <deantorkelson@utexas.edu>
2020-05-26 20:12:40 -0500

    added basic search functionality from frontend closes #3

--- frontend/src/Search/Search.tsx
+++ frontend/src/Search/Search.tsx
@@ -1,14 +1,36 @@
 import React from 'react'
+import SpotifyApiService from '../SpotifyApiService/SpotifyApiService'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-export class Search extends React.Component {
 
-  render() {
-    return (
-      <div>
-        TODO: create input field, pass to search service 
-      </div>
-    )
-  }
+let searchQuery: string = '';
+let spotifyApiService: SpotifyApiService = new SpotifyApiService();
+
+function searchSubmit(value: any) {
+  // console.log(searchQuery);
+  console.log(spotifyApiService.search(searchQuery).then(data => {
+    console.log(data)
+  }));
+}
+
+function Search() {
+  return (
+    <div>
+        Enter the name of the artist to search for:<br/>
+        <Form.Control
+          type="text"
+          placeholder="Enter an artist name..."
+          onChange={(value: any) =>
+            searchQuery = value.target.value
+          }
+        />
+        <Button variant="primary" type="submit" onClick={(event: any) => searchSubmit(event)}> 
+          Submit
+        </Button>
+
+    </div>
+  )
 }
 
 export default Search;
--- frontend/src/SpotifyApiService/SpotifyApiService.tsx
+++ frontend/src/SpotifyApiService/SpotifyApiService.tsx
@@ -1,12 +1,13 @@
 export class SpotifyApiService {
-
     private api_url: string;
+
     constructor() {
         this.api_url = 'http://127.0.0.1:5000'
     }
 
-    public search(searchQuery: string): Promise<Response> {
-        return fetch(this.api_url + `/search/${searchQuery}`)
+    public search(searchQuery: string): Promise<any> {
+        return fetch(this.api_url + `/search/${searchQuery}`).then(response => response.json());
     }
-
 }
+
+export default SpotifyApiService;
