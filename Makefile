8a6dc52e9cc5d8b601dd5124b6e298816f1668c3
deantorkelson <deantorkelson@utexas.edu>
2020-05-27 19:39:21 -0500

    updated makefiles, added cors support

---
+++ Makefile
@@ -0,0 +1,5 @@
+run-fe:
+	cd frontend/ && make run
+
+run-be:
+	cd backend/ && make run
--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from spotify import Spotify
 from flask import Flask
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 spotify = Spotify()
 
 @app.route('/search/<string:search_query>/')
+@cross_origin()
 def search(search_query):
    return spotify.search(search_query)
 
