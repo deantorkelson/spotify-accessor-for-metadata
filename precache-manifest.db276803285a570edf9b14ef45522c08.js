2e020a9231221a23fc4c3cd12989c279c63ab2ef
deantorkelson <deantorkelson@utexas.edu>
2020-06-24 10:23:52 -0500

    Updates

---
+++ precache-manifest.db276803285a570edf9b14ef45522c08.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce79ebca7d2f733ed22350fa29b65a12",
+    "url": "/spotify-accessor-for-metadata/index.html"
+  },
+  {
+    "revision": "69c9b067fa95985a1c88",
+    "url": "/spotify-accessor-for-metadata/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "6f3243392448dd6e4327",
+    "url": "/spotify-accessor-for-metadata/static/css/main.1a74d8d0.chunk.css"
+  },
+  {
+    "revision": "69c9b067fa95985a1c88",
+    "url": "/spotify-accessor-for-metadata/static/js/2.79934e97.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/spotify-accessor-for-metadata/static/js/2.79934e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3243392448dd6e4327",
+    "url": "/spotify-accessor-for-metadata/static/js/main.9a77113c.chunk.js"
+  },
+  {
+    "revision": "a9e0d62cccde95010706",
+    "url": "/spotify-accessor-for-metadata/static/js/runtime-main.09ff1f83.js"
+  },
+  {
+    "revision": "5e670a4b422ca91adbff670dbd8a9e1f",
+    "url": "/spotify-accessor-for-metadata/static/media/black-logo.5e670a4b.png"
+  },
+  {
+    "revision": "feb227e23cb29a0265c99611e36199a5",
+    "url": "/spotify-accessor-for-metadata/static/media/green-logo.feb227e2.png"
+  }
+]);
