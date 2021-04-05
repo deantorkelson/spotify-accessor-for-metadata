0348172b43f74f692c2500fd24e82a68d7398a3a
deantorkelson <deantorkelson@utexas.edu>
2021-04-05 10:13:10 -0500

    Add untracked files

---
+++ frontend/src/tests/helpers.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { isUriList } from '../helpers/helpers'
+
+test('valid: list of colon Spotify URIs', () => {
+  const uris = "spotify:playlist:0iAeUtwINlqfjwAyQ4ykur, spotify:playlist:0iAeUtwINlqfjwAyQ4ykur"
+  expect(isUriList(uris)).toEqual(true);
+});
+
+test('valid: long list of colon Spotify URIs', () => {
+  const uris = "spotify:playlist:0iAeUtwINlqfjwAyQ4ykur, spotify:playlist:0iAeUtwINlqfjwAyQ4ykur,spotify:playlist:0iAeUtwINlqfjwAyQ4ykur, spotify:playlist:0iAeUtwINlqfjwAyQ4ykur"
+  expect(isUriList(uris)).toEqual(true);
+});
+
+test('valid: list of forward slash Spotify URIs', () => {
+  const uris = "spotify/playlist/0iAeUtwINlqfjwAyQ4ykur, spotify/playlist/0iAeUtwINlqfjwAyQ4ykur"
+  expect(isUriList(uris)).toEqual(true);
+});
+
+test('valid: space separator', () => {
+  const uris = "spotify/playlist/0iAeUtwINlqfjwAyQ4ykur spotify:playlist:0iAeUtwINlqfjwAyQ4ykur"
+  expect(isUriList(uris)).toEqual(true);
+});
+
+test('invalid: empty string', () => {
+  const uris = ""
+  expect(isUriList(uris)).toEqual(false);
+});
+
+test('invalid: single element', () => {
+  const uris = "spotify:playlist:0iAeUtwINlqfjwAyQ4ykur"
+  expect(isUriList(uris)).toEqual(false);
+});
+
+test('invalid: too short', () => {
+  const uris = "spotify/playlist/0iAeUtwINlqfjwAyQ4ykur, spotify:0iAeUtwINlqfjwAyQ4ykur"
+  expect(isUriList(uris)).toEqual(false);
+});
+
+test('invalid: missing type', () => {
+  const uris = "spotify/playlist/0iAeUtwINlqfjwAyQ4ykur playlist:0iAeUtwINlqfjwAyQ4ykur"
+  expect(isUriList(uris)).toEqual(false);
+});
