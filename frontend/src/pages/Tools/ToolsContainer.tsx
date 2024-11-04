bb7f774f4522041ffde0727897003e9b484c47e2
Dean Torkelson <deantork45@gmail.com>
2024-11-03 23:59:16 -0500

    Add random practice tools

--- frontend/src/pages/Tools/ToolsContainer.tsx
+++ frontend/src/pages/Tools/ToolsContainer.tsx
@@ -3,12 +3,36 @@ import React, { useState, useEffect } from "react";
 import { BookQuoteResponse } from "src/models/api/BookQuoteResponse";
 import ToolsPresenter from "src/pages/Tools/ToolsPresenter";
 import SpotifyApiService from "src/utils/api/SpotifyApiService/SpotifyApiService";
+import { keyNumberToString } from "../../utils/helpers";
+
+const MINOR_PENTATONIC_DEGREES = [1, 3, 4, 5, 7];
 
 export const RubinContainer = () => {
   const spotifyApiService = new SpotifyApiService();
 
   const [rubinQuote, setRubinQuote] = useState("");
   const [bookQuote, setBookQuote] = useState({} as BookQuoteResponse);
+  const [scaleDegree, setScaleDegree] = useState(1);
+  const [tonality, setTonality] = useState("E");
+  const [randomEnabled, setRandomEnabled] = useState(false);
+  const [shouldUseFullScale, setShouldUseFullScale] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (randomEnabled) {
+        setTonality(keyNumberToString(Math.floor(Math.random() * 11)));
+        if (shouldUseFullScale) {
+          setScaleDegree(Math.floor(Math.random() * 7) + 1);
+        } else {
+          setScaleDegree(
+            MINOR_PENTATONIC_DEGREES[Math.floor(Math.random() * 5)]
+          );
+        }
+      }
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, [randomEnabled, shouldUseFullScale]);
 
   useEffect(() => {
     spotifyApiService.rubinQuote().then((data) => {
@@ -20,7 +44,18 @@ export const RubinContainer = () => {
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <ToolsPresenter bookQuote={bookQuote} rubinQuote={rubinQuote} />;
+  return (
+    <ToolsPresenter
+      bookQuote={bookQuote}
+      rubinQuote={rubinQuote}
+      scaleDegree={scaleDegree}
+      tonality={tonality}
+      randomEnabled={randomEnabled}
+      setRandomEnabled={setRandomEnabled}
+      shouldUseFullScale={shouldUseFullScale}
+      setShouldUseFullScale={setShouldUseFullScale}
+    />
+  );
 };
 
 export default RubinContainer;
--- frontend/src/pages/Tools/ToolsPresenter.tsx
+++ frontend/src/pages/Tools/ToolsPresenter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Checkbox } from "@material-ui/core";
 
 import { BookQuoteResponse } from "src/models/api/BookQuoteResponse";
 import "./Tools.css";
@@ -6,9 +7,25 @@ import "./Tools.css";
 interface Props {
   rubinQuote: string;
   bookQuote: BookQuoteResponse;
+  scaleDegree: number;
+  tonality: string;
+  randomEnabled: boolean;
+  setRandomEnabled: (value: boolean) => void;
+  shouldUseFullScale: boolean;
+  setShouldUseFullScale: (value: boolean) => void;
 }
+
 export const ToolsPresenter = (props: Props) => {
-  const { rubinQuote, bookQuote } = props;
+  const {
+    rubinQuote,
+    bookQuote,
+    scaleDegree,
+    tonality,
+    randomEnabled,
+    setRandomEnabled,
+    shouldUseFullScale,
+    setShouldUseFullScale,
+  } = props;
 
   return (
     <div className="page">
@@ -27,11 +44,43 @@ export const ToolsPresenter = (props: Props) => {
           <i>{bookQuote.author}</i>
         </span>
         <hr />
-        <h1 className="header">Music Tools</h1>
-        <h2>A small set of tools that I use for music practice.</h2>
+        <h1 className="header">Music tools</h1>
+        <p>
+          A small set of tools that I use for music practice. I use these random
+          values for different freboard/scale/mode exercises, like "Play the
+          pentatonic minor scale in the given key starting on the given scale
+          degree"
+        </p>
+        <div>
+          <Checkbox
+            onClick={(event) => {
+              setRandomEnabled(!randomEnabled);
+            }}
+            value={randomEnabled}
+            color="primary"
+          />
+          Enable random values?
+        </div>
+        <div>
+          <Checkbox
+            onClick={(event) => {
+              setShouldUseFullScale(!shouldUseFullScale);
+            }}
+            value={shouldUseFullScale}
+            color="primary"
+          />
+          Enable full scale? (default is pentatonic)
+        </div>
         <div className="main-content">
           <div className="column">
-            <h3>Random Key</h3>
+            <div></div>
+            <h4>Random Key</h4>
+            <div className="random-value">{tonality}</div>
+          </div>
+          <div className="column">
+            <div></div>
+            <h4>Random Scale Degree</h4>
+            <div className="random-value">{scaleDegree}</div>
           </div>
         </div>
       </div>
